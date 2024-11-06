1ec1d4024194d1b23335555aeec5c0b6faf73838
dev7odaa <mah.khalil22@gmail.com>
2024-11-06 14:43:36 +0200

    edit MainActivity

--- app/src/main/java/com/example/weatherapp/Presentation/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/Presentation/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.weatherapp.Presentation
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.weatherapp.Presentation.screens.WeatherScreen
@@ -12,6 +14,7 @@ import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -24,6 +27,7 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@RequiresApi(Build.VERSION_CODES.O)
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
